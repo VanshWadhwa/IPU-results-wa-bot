0ac400c8320ff6aac1ee846e5ddf49d7cf818702
Vansh Wadhwa <vanshwadhwasphere@gmail.com>
2022-08-30 18:33:35 +0530

    Api get data done

--- index.js
+++ index.js
@@ -11,5 +11,58 @@ const resultURL = API_URL + "results/" + demoRollNo;
 
 // Basic axios get
 axios.get(resultURL).then((res) => {
-  console.log(res.data);
+  //   console.log(res.data);
+  const result = res.data.data;
+
+  const introMsg = `
+ROLL NUMBER : ${result.rollNumber}
+NAME	: ${result.name}
+PROGRAMME : ${result.programme.name}
+INSTITUTION	: ${result.institution.name}
+BATCH	: ${result.batch}
+AGGREGATE PERCENTAGE : ${result.name}
+AGGREGATE CREDIT PERCENTAGE	: ${result.aggregateCreditPercentage}
+CREDITS	: ${result.maxCredits} / ${result.totalCreditsEarned} 
+
+  `;
+
+  console.log(introMsg);
+  var semMsgList = [];
+
+  var resultsLength = res.data.data.results.length;
+
+  for (var i = 0; i < resultsLength; i++) {
+    const currentSem = res.data.data.results[i];
+    // console.log(currentSem.semYear.num);
+    //Do something
+    const semMsg = `
+-----------------------------------------------------
+SEMESTER : ${currentSem.semYear.num}
+Percentage : ${currentSem.percentage}
+Total Marks :  ${currentSem.totalMarks}
+College Rank :  ${currentSem.collegeRank}
+University Rank :  ${currentSem.universityRank}
+Credit Percentage :  ${currentSem.creditPercentage}
+------------------------------------------------------
+`;
+    const subjects = currentSem.subjects;
+
+    console.log(semMsg);
+    const infoMsg = `
+        Sem-${currentSem.semYear.num} marks subject wise
+        I : Internal Marks
+        E : External Marks
+        T : Total Marks
+    `;
+    console.log(infoMsg);
+    for (var j = 0; j < subjects.length; j++) {
+      const currentSubject = subjects[j];
+      const subjectMsg = `${j + 1}. ${currentSubject.name} : I:${
+        currentSubject.minor.earned
+      }/${currentSubject.minor.max} |  E:${currentSubject.major.earned}/${
+        currentSubject.major.max
+      } |  T:${currentSubject.total.earned}/${currentSubject.total.max} `;
+      console.log(subjectMsg);
+    }
+  }
 });
